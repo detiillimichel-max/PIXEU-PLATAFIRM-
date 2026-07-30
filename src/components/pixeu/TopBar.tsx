0fb32d91bb55a1269a86544bbb0db0a01ce52279
Michel Detiilli <detiillimichel@gmail.com>
2026-07-29 21:35:08 -0300

    feat(pixeu): adicionar TopBar base

---
+++ src/components/pixeu/TopBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function TopBar() {
+  return (
+    <header className="pixeu-topbar" role="banner">
+      <div className="pixeu-topbar__brand">PIXEU</div>
+    </header>
+  );
+}
